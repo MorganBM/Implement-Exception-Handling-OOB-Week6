ee45f57990e0dffffc748fe41373646f93683220
MORGANBUCKLEYMCNAMAR <g00420537@atu.ie>
2024-10-23 17:37:45 +0100

    Step 6, on an invalid input allows you to try again

--- src/main/java/ie/atu/week6/UsingTryCatch.java
+++ src/main/java/ie/atu/week6/UsingTryCatch.java
@@ -3,26 +3,33 @@ package ie.atu.week6;
 import java.util.Scanner;
 public class UsingTryCatch {
     public static void main(String[] args) {
-        System.out.println("Please enter a number between 0 - 9 :");
-        // Check if the number is between 0 and 9
-        try (Scanner myscan = new Scanner(System.in)){
-            // Read user input as a String
-            String input = myscan.nextLine();
-            // Parse the input to an integer
-            int myNum = Integer.parseInt(input);
+        try(Scanner myscan = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("Please enter a number between 0 - 9 :");
+                // Check if the number is between 0 and 9
+                try {
 
-            if (myNum >= 0 && myNum <= 9) {
-                System.out.println("You entered " + myNum);
+                    // Read user input as a String
+                    String input = myscan.nextLine();
+                    // Parse the input to an integer
+                    int myNum = Integer.parseInt(input);
+
+                    if (myNum >= 0 && myNum <= 9) {
+                        System.out.println("You entered " + myNum);
+                        break;
+                    } else {
+                        System.out.println("Not a valid number, try again");
+                    }
+                } catch (NumberFormatException nfe) {
+                    System.out.println("Not a valid number, try again");
+
+                }
             }
-            else {
-                System.out.println("Not a valid number");
-            }
-        }
-        catch(NumberFormatException nfe) {
-            System.out.println("Not a valid number");
         }
     }
+
 }
+
 
 
 
